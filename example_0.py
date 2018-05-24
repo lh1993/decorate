b834ee9f038117e3b4771c39242a8e6c3ac2feae
lh1993 <lhln0119@163.com>
2018-05-24 14:21:30 +0800

    Create example_0.py

---
+++ example_0.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/23 15:28
+# @Author  : JY.Liu
+# @Site    : 
+# @File    : example_0.py
+# @Software: PyCharm
+
+def say_hello():
+    print("[Debug]: call function: say_hello()")
+    print("hello...")
+
+
+def say_goodby():
+    print("[Debug]: call function: say_goodby()")
+    print("goodby...")
+
+if __name__ == "__main__":
+    say_hello()
+    say_goodby()
